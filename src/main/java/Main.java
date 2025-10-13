562664f9f4476cb951b8537289b813b2eab0034f
Максим <maks.ohrimenko03032006@gmail.com>
2025-10-13 16:51:45 +0300

    changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,62 +2,61 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        // змінні різних типів
-
+        // Змінні різних типів
         String name = "Максим";
         int age = 19;
         double height = 1.83;
         boolean isStudent = true;
 
         // Вивід значень змінних
-        System.out.println("ім'я:" + name);
-        System.out.println("Вік" + age);
-        System.out.println("ЗРІСТ" + height + "m");
-        System.out.println("Студент" + isStudent);
+        System.out.println("Ім'я: " + name);
+        System.out.println("Вік: " + age);
+        System.out.println("Зріст: " + height + " m");
+        System.out.println("Студент: " + isStudent);
 
         // Умова if / else
         if (age < 18) {
-            System.out.println("Доступ Заборонено");
+            System.out.println("Доступ заборонено");
         } else {
             System.out.println("Доступ дозволено");
         }
 
-        // Контрукція switch
+        // Конструкція switch
         String day = "Monday";
         switch (day) {
             case "Saturday":
-            case "Sanday":
+            case "Sunday":
+                System.out.println("Вихідний день");
+                break;
+            default:
                 System.out.println("Робочий день");
                 break;
         }
 
-        // циккол for
+        // Цикл for
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Число" + i);
+            System.out.println("Число: " + i);
         }
 
-        // пошук елемента
+        // Пошук елемента
         int[] numbers = {3, 7, 2, 9, 5};
         int target = 3;
         int index = linearSearch(numbers, target);
 
         if (index != -1) {
-            System.out.println("Елемент знайдено на позиції :" + index);
+            System.out.println("Елемент знайдено на позиції: " + index);
         } else {
             System.out.println("Елемент не знайдено");
-
         }
     }
 
-    // пошук елеманта в масиві
-
-    public static int linearSearch ( int[] arr, int target){
-        for ( int i = 0; i < arr.length; i++) {
+    // Пошук елемента в масиві
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 return i;
             }
         }
-        return  -1;
+        return -1;
     }
 }
-///////// comment/////
